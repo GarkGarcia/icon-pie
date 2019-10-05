706165c34fe4e6f7b41e723ea7af76a70591f469
Gark Garcia <37553739+GarkGarcia@users.noreply.github.com>
2019-10-05 20:13:23 -0300

    Fixed the parser.

--- src/parse/combinators.rs
+++ src/parse/combinators.rs
@@ -3,91 +3,78 @@ use super::{Token, TokenStream, Flag};
 use std::{path::PathBuf, iter::Iterator};
 use icon_baker::Icon;
 
-pub fn parse<
+pub fn icon<
     I: Icon,
     F: 'static + FnMut(Entries<<I as Icon>::Key>, Output) -> Command,
     G: FnMut(u32) -> Option<<I as Icon>::Key>
 >(
-    constructor: F,
-    converter: G
-) -> impl Fn(&mut TokenStream, usize) -> Result<Command, Error> {
-    |it, n_entries| {
-        let mut entries: Entries<<I as Icon>::Key> = Vec::with_capacity(n_entries);
-        it.next();
-    
-        while let Some(&(c, token)) = it.peek() {
-            match token {
-                Token::Flag(Flag::Entry) => entry(keys(converter))(it, &mut entries)?,
-                Token::Flag(Flag::Output) => return construct(constructor)(it, entries),
-                _ => return syntax!(SyntaxError::UnexpectedToken(c))
-            }
-        }
+    mut constructor: F,
+    mut converter: G,
+    it: &mut TokenStream,
+    n_entries: usize
+) -> Result<Command, Error> {
+    let mut entries: Entries<<I as Icon>::Key> = Vec::with_capacity(n_entries);
+    it.next();
 
-        Ok(constructor(entries, Output::Stdout))
+    while let Some(&(c, token)) = it.peek() {
+        match token {
+            Token::Flag(Flag::Entry) => entry::<I, _>(|size| converter(size), it, &mut entries)?,
+            Token::Flag(Flag::Output) => return expect_pathed_output::<I, _>(constructor, it, entries),
+            _ => return syntax!(SyntaxError::UnexpectedToken(c))
+        }
+    }
+
+    Ok(constructor(entries, Output::Stdout))
+}
+
+pub fn entry<I: Icon, F: FnMut(u32) -> Option<<I as Icon>::Key>>(
+    adder: F,
+    it: &mut TokenStream,
+    entries: &mut Entries<<I as Icon>::Key>
+) -> Result<(), Error> {
+    it.next();
+    match it.peek() {
+        Some(&(_, Token::Path(path))) => keys::<I, _>(adder, it, entries, path),
+        Some(&(c, _)) => syntax!(SyntaxError::UnexpectedToken(c)),
+        None => syntax!(SyntaxError::UnexpectedEnd)
     }
 }
 
-pub fn entry<
-    I: Icon,
-    F: 'static + FnMut(&mut TokenStream, &mut Entries<<I as Icon>::Key>, &PathBuf) -> Result<(), Error>
->(
-    mut adder: F,
-) -> impl FnMut(&mut TokenStream, &mut Entries<<I as Icon>::Key>) -> Result<(), Error> {
-    move |it, entries| {
-        it.next();
-        match it.peek() {
-            Some(&(_, Token::Path(path))) => adder(it, entries, path),
-            Some(&(c, _)) => syntax!(SyntaxError::UnexpectedToken(c)),
-            None => syntax!(SyntaxError::UnexpectedEnd)
-        }
-    }
-}
+fn keys<I: Icon, F: FnMut(u32) -> Option<<I as Icon>::Key>>(
+    mut converter: F,
+    it: &mut TokenStream,
+    entries: &mut Entries<<I as Icon>::Key>,
+    path: &PathBuf
+) -> Result<(), Error> {
+    // TODO Preallocate this Vec
+    let mut sizes = Vec::with_capacity(0);
 
-pub fn construct<I: Icon, F: 'static + FnMut(Entries<<I as Icon>::Key>, Output) -> Command>(
-    mut cons: F
-) -> impl FnMut(&mut TokenStream, Entries<<I as Icon>::Key>) -> Result<Command, Error> {
-    move |it, entries| {
-        it.next();
-        match it.peek() {
-            Some(&(_, Token::Path(path))) => expect_end(it, cons(entries, Output::Path(path.clone()))),
-            Some(&(c, _)) => syntax!(SyntaxError::UnexpectedToken(c)),
-            None => syntax!(SyntaxError::UnexpectedEnd)
-        }
-    }
-}
-
-pub fn keys<I: Icon, F: FnMut(u32) -> Option<<I as Icon>::Key>>(
-    mut f: F
-) -> impl FnMut(&mut TokenStream, &mut Entries<<I as Icon>::Key>, &PathBuf) -> Result<(), Error> {
-    move |it, entries, path| {
-        // TODO Preallocate this Vec
-        let mut sizes = Vec::with_capacity(0);
-    
-        it.next();
-        match it.peek() {
-            Some(&(_, Token::Size(_))) => while let Some(&(_, Token::Size(size))) = it.peek() {
+    it.next();
+    match it.peek() {
+        Some(&(_, Token::Size(_))) => {
+            while let Some(&(_, Token::Size(size))) = it.peek() {
                 it.next();
                 sizes.push(*size);
-            },
-            Some(&(c, _)) => return syntax!(SyntaxError::UnexpectedToken(c)),
-            None          => return syntax!(SyntaxError::UnexpectedEnd)
-        }
-    
-        let filter = filter(it)?;
-    
-        for size in sizes {
-            if let Some(key) = f(size) {
-                entries.push((key, path.clone(), filter));
-            } else {
-                return Err(Error::InvalidDimensions(size));
             }
-        }
-    
-        Ok(())
+        },
+        Some(&(c, _)) => return syntax!(SyntaxError::UnexpectedToken(c)),
+        None => return syntax!(SyntaxError::UnexpectedEnd)
     }
+
+    let filter = filter(it)?;
+
+    for size in sizes {
+        if let Some(key) = converter(size) {
+            entries.push((key, path.clone(), filter));
+        } else {
+            return Err(Error::InvalidDimensions(size));
+        }
+    }
+
+    Ok(())
 }
 
-pub fn filter(it: &mut TokenStream) -> Result<ResamplingFilter, Error> {
+fn filter(it: &mut TokenStream) -> Result<ResamplingFilter, Error> {
     if let Some((_, Token::Flag(Flag::Resample))) = it.peek() {
         it.next();
         match it.peek() {
@@ -106,5 +93,18 @@ pub fn expect_end(it: &mut TokenStream, command: Command) -> Result<Command, Err
     match it.peek() {
         Some(&(c, _)) => syntax!(SyntaxError::UnexpectedToken(c)),
         None => Ok(command)
+    }
+}
+
+fn expect_pathed_output<I: Icon, F: 'static + FnMut(Entries<<I as Icon>::Key>, Output) -> Command>(
+    mut constructor: F,
+    it: &mut TokenStream,
+    entries: Entries<<I as Icon>::Key>
+) -> Result<Command, Error> {
+    it.next();
+    match it.peek() {
+        Some(&(_, Token::Path(path))) => expect_end(it, constructor(entries, Output::Path(path.clone()))),
+        Some(&(c, _)) => syntax!(SyntaxError::UnexpectedToken(c)),
+        None => syntax!(SyntaxError::UnexpectedEnd)
     }
 }
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,12 +1,12 @@
 use crate::{command::Command, syntax, error::{Error, SyntaxError}};
 use std::{iter::{Iterator, Peekable, Enumerate}, slice::Iter};
-use icon_baker::{Icon, ico::Ico, icns::Icns, favicon::Favicon};
+use icon_baker::{Icon, ico::{Ico, IcoKey}, icns::Icns, favicon::Favicon};
 
 mod combinators;
 mod token;
 
 use token::{Flag, Token, Cmd};
-use combinators::{parse, expect_end};
+use combinators::{icon, expect_end};
 
 type TokenStream<'a> = Peekable<Enumerate<Iter<'a, Token>>>;
 
@@ -37,23 +37,29 @@ fn tokens<'a>(args: Vec<String>) -> Vec<Token> {
     args.iter().map(|arg| Token::from(arg.as_ref())).collect()
 }
 
+#[inline]
 fn favicon(it: &mut TokenStream, n_entries: usize) -> Result<Command, Error> {
-    parse::<Favicon, _, _>(
+    icon::<Favicon, _, _>(
         Command::Favicon,
-        <Favicon as Icon>::Key::new
-    )(it, n_entries)
+        <Favicon as Icon>::Key::new,
+        it, n_entries
+    )
 }
 
+#[inline]
 fn icns(it: &mut TokenStream, n_entries: usize) -> Result<Command, Error> {
-    parse::<Icns, _, _>(
+    icon::<Icns, _, _>(
         Command::Icns,
-        <Icns as Icon>::Key::from
-    )(it, n_entries)
+        <Icns as Icon>::Key::from,
+        it, n_entries
+    )
 }
 
+#[inline]
 fn ico(it: &mut TokenStream, n_entries: usize) -> Result<Command, Error> {
-    parse::<Ico, _, _>(
+    icon::<Ico, _, _>(
         Command::Ico,
-        |size| if size < 256 { <Ico as Icon>::Key::new(size as u8) } else { None }
-    )(it, n_entries)
+        |size| if size < 256 { IcoKey::new(size as u8) } else { None },
+        it, n_entries
+    )
 }
